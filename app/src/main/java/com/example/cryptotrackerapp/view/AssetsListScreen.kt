3e21c11df0e3ed203eab0afff985c1ae6e662119
aarizaga85 <andresarizaga@gmail.com>
2024-03-23 22:01:17 -0500

    Manejo de paquetes y assets

---
+++ app/src/main/java/com/example/cryptotrackerapp/view/AssetsListScreen.kt
@@ -0,0 +1,121 @@
+package com.example.cryptotrackerapp.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.cryptotrackerapp.model.Asset
+
+@Composable
+fun AssetList (){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.onBackground)
+    ) {
+        AssetRow(
+            Asset(
+                id = "1",
+                name = "Ethereum",
+                symbol = "ETH",
+                price = 18000.0,
+                percentage = -1.5
+            )
+        )
+        Divider()
+        AssetRow(
+            Asset(
+                id = "2",
+                name = "Bitcoin",
+                symbol = "BTC",
+                price = 65000.0,
+                percentage = 18.0
+            )
+        )
+    }
+}
+@Composable
+fun AssetRow(asset: Asset){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp)
+    ) {
+
+
+        Box(modifier = Modifier.padding(horizontal = 8.dp)){
+            if (LocalInspectionMode.current){
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier
+                        .size(30.dp)
+                )
+            } else{
+                AsyncImage(
+                    model = "https://assets.coincap.io/assets/icons/${asset.symbol.lowercase()}@2x.png",
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(30.dp)
+                )
+            }
+        }
+
+        Column {
+            Text(
+                text = asset.name,
+                fontSize = 16.sp,
+                color = Color.White
+            )
+            Text(
+                text = asset.symbol,
+                fontSize = 14.sp,
+                color = Color.Gray
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            text = "$${asset.price}",
+            fontSize = 14.sp,
+            color = Color.White,
+            modifier = Modifier
+                .padding(8.dp)
+        )
+        Text(
+            text = "${asset.percentage}%",
+            color = if (asset.percentage > 0) Color.Green else Color.Red,
+            fontSize = 14.sp,
+            modifier = Modifier
+                .padding(8.dp)
+        )
+    }
+}
+
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true)
+@Composable
+fun AssetRowPreview(){
+    AssetList()
+}
